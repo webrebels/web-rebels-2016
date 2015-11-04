5bad7b68038f58204b88723ea56512de7f5c0ac3
leftieFriele <leftie.friele@gmail.com>
2015-11-04 14:03:50 +0100

    adding support for testing subdomains locally

--- bin/routes/archives.js
+++ bin/routes/archives.js
@@ -2,9 +2,13 @@
 var vhost = require('vhost');
 var connect = require('connect');
 var servestatic = require('serve-static')
+var config = require('../../config');
 
 module.exports = (app) => {
   ['2012', '2013', '2014', '2015'].map((year) => {
     app.use(vhost(`${year}.webrebels.org`, connect().use(servestatic(`./node_modules/web-rebels-${year}/`))));
+    if (config.get('env') === 'development') {
+      app.use(vhost(`${year}.localhost`, connect().use(servestatic(`./node_modules/web-rebels-${year}/`))));
+    }
   });
 };
