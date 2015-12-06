9b840fce4cf3343606f45b172db422d9eac854ac
leftieFriele <leftie.friele@gmail.com>
2015-12-06 20:41:31 +0100

    adjusted the about page

--- bin/routes/content.js
+++ bin/routes/content.js
@@ -1,36 +1,46 @@
-var fs                  = require('fs');
-var path                = require('path');
-var config              = require('../../config');
+var fs = require('fs');
+var path = require('path');
+var config = require('../../config');
 
-var css = fs.readFileSync(path.resolve(__dirname, '../..' + config.get('docRoot') + '/css/structure.css'), {encoding:'utf8'});
+var css = fs.readFileSync(path.resolve(__dirname, '../..' + config.get(
+  'docRoot') + '/css/structure.css'), {
+  encoding: 'utf8'
+});
 
 module.exports = (app) => {
   app.get('/', (req, res) => {
-      res.render('waiting', {
-          pageTitle: 'Web Rebels ☠ Oslo ☠ June 2016',
-          header: 'penthouse',
-          css: css
-      });
+    res.render('waiting', {
+      pageTitle: 'Web Rebels ☠ Oslo ☠ June 2016',
+      header: 'penthouse',
+      css: css
+    });
   });
   app.get('/home', (req, res) => {
-      res.render('home', {
-          pageTitle: 'Web Rebels ☠ Oslo ☠ June 2016',
-          header: 'penthouse',
-          css: css
-      });
+    res.render('home', {
+      pageTitle: 'Web Rebels ☠ Oslo ☠ June 2016',
+      header: 'penthouse',
+      css: css
+    });
+  });
+  app.get('/about', (req, res) => {
+    res.render('about', {
+      pageTitle: 'About the Web Rebels Conference',
+      header: 'penthouse',
+      css: css
+    });
   });
   app.get('/sponsors/packages', (req, res) => {
-      res.render('sponsors/packages', {
-          pageTitle: 'Web Rebels Sponsorship options 2016',
-          header: 'penthouse',
-          css: css
-      });
+    res.render('sponsors/packages', {
+      pageTitle: 'Web Rebels Sponsorship options 2016',
+      header: 'penthouse',
+      css: css
+    });
   });
   app.get('/scholarship', (req, res) => {
-      res.render('scholarship/index', {
-          pageTitle: 'Web Rebels Scholarship Programme',
-          header: 'penthouse',
-          css: css
-      });
+    res.render('scholarship/index', {
+      pageTitle: 'Web Rebels Scholarship Programme',
+      header: 'penthouse',
+      css: css
+    });
   });
 };
